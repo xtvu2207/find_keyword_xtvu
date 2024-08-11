64122f492da00d18c7ce0fbfef54d2daa63a46c7
Xuan Tung VU <xuantungvu@MBP-de-Xuan.lan>
2024-08-12 01:18:44 +0200

    Update

--- build/lib/find_keyword_xtvu/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-
-from .core import find_keyword_xtvu  # Importation de la fonction depuis le fichier core.py
-
-__all__ = ["find_keyword_xtvu"]  # Définit ce qui est exposé lorsque le module est importé
